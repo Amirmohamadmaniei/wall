285720ce45c8fff463e229310a13acb82dce4a02
Amir Mohamad Maniei <110853819+Amirmohamadmaniei@users.noreply.github.com>
2023-04-02 15:48:57 +0330

    first

--- wall/settings.py
+++ wall/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$$f4^5c@cukxu2rtda%t=21rd94x4f!5agq9p(585=@ot%8!5i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
